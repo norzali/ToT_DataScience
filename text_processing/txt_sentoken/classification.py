da3a5b1c2dbf6e37d54faccf1cd8a02878abf4f8
Akhlaq Ahmad <33060835+akhlaqahmad@users.noreply.github.com>
2020-09-10 00:59:11 +0800

    text processing
    -data set
    -code

---
+++ text_processing/txt_sentoken/classification.py
@@ -0,0 +1,44 @@
+import numpy as np
+import re
+import nltk
+from sklearn.datasets import load_files
+import pickle
+from nltk.corpus import stopwords
+
+
+movie_data = load_files(r"C:\Users\Lenovo\Downloads\review_polarity\txt_sentoken")
+X, y = movie_data.data, movie_data.target
+
+
+documents = []
+
+from nltk.stem import WordNetLemmatizer
+
+stemmer = WordNetLemmatizer()
+
+for sen in range(0, len(X)):
+    # Remove all the special characters
+    document = re.sub(r'\W', ' ', str(X[sen]))
+
+    # remove all single characters
+    document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
+
+    # Remove single characters from the start
+    document = re.sub(r'\^[a-zA-Z]\s+', ' ', document)
+
+    # Substituting multiple spaces with single space
+    document = re.sub(r'\s+', ' ', document, flags=re.I)
+
+    # Removing prefixed 'b'
+    document = re.sub(r'^b\s+', '', document)
+
+    # Converting to Lowercase
+    document = document.lower()
+
+    # Lemmatization
+    document = document.split()
+
+    document = [stemmer.lemmatize(word) for word in document]
+    document = ' '.join(document)
+
+    documents.append(document)
